e3b4ccb08154620517961101242e7a8888ebf193
XTU3xiaoxin <lean0123@163.com>
2014-07-30 17:51:26 +0800

    just test merge
    just test merge

--- src/main/java/com/tja/Main.java
+++ src/main/java/com/tja/Main.java
@@ -10,6 +10,7 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 
+		/**  add some comment to test */
 		EntityManagerFactory factory = Persistence.createEntityManagerFactory("myJPA");  
          factory.close();
 	}
