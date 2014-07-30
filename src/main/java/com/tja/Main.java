59a46ca9aaef4cb1736d373b8bd84f028dd4eba8
XTU3xiaoxin <lean0123@163.com>
2014-07-30 17:53:22 +0800

    test
    test

--- src/main/java/com/tja/Main.java
+++ src/main/java/com/tja/Main.java
@@ -10,7 +10,6 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 
-		/**  add some comment to test */
 		EntityManagerFactory factory = Persistence.createEntityManagerFactory("myJPA");  
          factory.close();
 	}
